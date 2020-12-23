d42c1e6bc6c1f7d9c31c10cfb2cee38e5befb698
nxshen <michael7105@gmail.com>
2020-12-23 18:07:59 +0800

    test

--- apache/data/pi/index.php
+++ apache/data/pi/index.php
@@ -1,30 +1,57 @@
 <?php
-$server = "mariadb";         # MySQL/MariaDB 伺服器
-$dbuser = "root";       # 使用者帳號
-$dbpassword = $_ENV["MYSQL_ROOT_PASSWORD"]; # 使用者密碼
-$dbname = "mariadb";    # 資料庫名稱
+  $server = "mariadb";         # MySQL/MariaDB 伺服器
+  $dbuser = "root";       # 使用者帳號
+  $dbpassword = $_ENV["MYSQL_ROOT_PASSWORD"]; # 使用者密碼
+  $dbname = "mariadb";    # 資料庫名稱
+  
 
-# 連接 MySQL/MariaDB 資料庫
-$connection = new mysqli($server, $dbuser, $dbpassword, $dbname);
+  // 面向過程
+  $mysqli = mysqli_connect($server, $dbuser, $dbpassword, $dbname);
 
-# 檢查連線是否成功
-if ($connection->connect_error) {
-  die("連線失敗：" . $connection->connect_error);
-}
+  if(mysqli_connect_errno($mysqli)){
+      echo "Failed to connect to MySql: ".mysqli_connect_error();
+      return;
+  }
+  else{
+    echo "Successfully connect to MySql: ".mysqli_connect_error();
+  }
 
-# MySQL/MariaDB 指令
-#$sqlQuery = "CREATE TABLE user_table (
-#  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-#  name VARCHAR(50),
-#  age INT UNSIGNED)";
+  $res = mysqli_query($mysqli,"select '*' as _msg from parking_space");
+  $row = mysqli_fetch_assoc($res);
+  echo $row["_msg"]."<br>";
+  mysqli_close($mysqli);
 
-# 執行 MySQL/MariaDB 指令
-#if ($connection->query($sqlQuery) === TRUE) {
-#  echo "成功建立資料表。";
-#} else {
-#  echo "執行失敗：" . $connection->error;
-#}
+  # 查詢資料
+  $select_result = $mysqli03->query("select '*' from parking_space");
+  print_r($select_result);
+  echo "<br>";
 
-# 關閉 MySQL/MariaDB 連線
-$connection->close();
+  # 遍歷結果,第一種遍歷方法
+  echo "<p>第一種遍歷方法：</p>";
+  for( $row_no=0; $row_no<$select_result->num_rows; $row_no++)
+  {
+      $select_result->data_seek($row_no);
+      $rows = $select_result->fetch_assoc();
+
+      foreach($rows as $key=>$value)
+      {
+          echo "$key:$value ";
+      }
+      echo "<br>";
+  }
+
+  # 遍歷查詢結果，第二種遍歷方法
+  $select_result->data_seek(0);
+  echo "<p>第二種遍歷方法：</p>";
+
+  while( $row=$select_result->fetch_assoc())
+  {
+      echo " camera_id= ".$row["camera_id"]." lisence_plate=".$row["lisence_plate"];
+      echo "<br>";
+  }
+  echo "<br>";
+
+  # 關閉連線
+  $mysqli03->close();
+    
 ?>
