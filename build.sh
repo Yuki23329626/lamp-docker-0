0cbefc8e0e0df34aa54a94b284a38a4591b9af3a
nxshen <michael7105@gmail.com>
2020-12-23 09:23:10 +0000

    update

--- build.sh
+++ build.sh
@@ -5,7 +5,14 @@
 # 2020/09/26	nxshen	First release
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 
-echo "building container: apache"
-docker-compose up --build -d apache
+if [ "$MYSQL_ROOT_PASSWORD" = "" ]
+then
+  echo "\nYou should set your MYSQL_ROOT_PASSWORD at first"
+  echo "\ne.g.: export MYSQL_ROOT_PASSWORD=\"the_password_you_want\"\n"
+  exit 0
+fi
+
+echo "building container..."
+docker-compose up --build -d
 
 exit 0
