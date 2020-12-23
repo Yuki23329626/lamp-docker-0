5214eaa18b924fc27387367928dc87fa6fd8fff8
nxshen <michael7105@gmail.com>
2020-12-23 17:45:25 +0800

    update init.sql

--- mariadb/init.sql
+++ mariadb/init.sql
@@ -1,14 +1,15 @@
+-- 初始化 mariadb database 使用的 sql
+
 DROP DATABASE IF EXISTS `pi_parking_monitor`;
 CREATE DATABASE `pi_parking_monitor` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
-USE `pi_parking_monitor`;i
+USE `pi_parking_monitor`;
 
-DROP TABLE IF EXISTS ``;
-CREATE TABLE `department` (
-  `id` int(5) NOT NULL,
-  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS `parking_space`;
+CREATE TABLE `parking_space` (
+  `camera_id` int(5) NOT NULL,
+  `lisence_plate` text COLLATE utf8mb4_unicode_ci ,
+  PRIMARY KEY (`camera_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `department` (`id`, `name`) VALUES
-(5201,	'資訊工程學系');
-
+INSERT INTO `parking_space` (`camera_id`, `lisence_plate`) VALUES
+(0001,	'');
