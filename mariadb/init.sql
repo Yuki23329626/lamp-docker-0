ec0f51226d410884e4fb6251c0bb07ec654918bc
nxshen <michael7105@gmail.com>
2020-12-24 12:48:16 +0800

    update

--- mariadb/init.sql
+++ mariadb/init.sql
@@ -1,22 +1,8 @@
--- these sqls are used to initialize database and table
-
-DROP DATABASE IF EXISTS `pi_parking_monitor`;
-CREATE DATABASE `pi_parking_monitor` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 
 USE `pi_parking_monitor`;
 DROP TABLE IF EXISTS `parking_space`;
 CREATE TABLE `parking_space` (
-  `camera_id` VARCHAR(10) NOT NULL ,
+  `camera_id` CHAR(10) NOT NULL ,
   `lisence_plate` text COLLATE utf8mb4_unicode_ci ,
   PRIMARY KEY (`camera_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-INSERT INTO `parking_space` (`camera_id`, `lisence_plate`) VALUES
-('A01',	'ABC1234'),
-('A02',	'ASD7365'),
-('A03',	'FJO4877'),
-('A04',	'DBL2846'),
-('A05',	'DIH8493'),
-('A06',	'XOID384'),
-('A07',	'MXN2093'),
-('A08',	'VVE9987');
--- mariadb/readme.md
+++ mariadb/readme.md
@@ -16,5 +16,8 @@ docker exec -it $container_id /bin/bash
 mysql --local-infile -u root -p 'pi_parking_monitor' --default-character-set=utf8mb4
 # you will need to type your mysql root password later  
 ```
-
+## Note
+1. 不知道為啥 adminer 不能處理 primary key type 為 varchar 的 資料  
+在 create table 的時候會出問題  
+可能要考慮在 mysql 的 cli 
 
