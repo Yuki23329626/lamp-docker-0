cd741861c89af4182ee1f3f7be993f6050d24852
nxshen <michael7105@gmail.com>
2021-01-18 22:03:08 +0800

    update

--- apache/data/pi/carLocationSearch/Manager/location_query_all.php
+++ apache/data/pi/carLocationSearch/Manager/location_query_all.php
@@ -5,8 +5,8 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/carLocationSearch/Foundation/HelpUtil.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/carLocationSearch/Foundation/HttpReq.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . 'pi/carLocationSearch/Foundation/HelpUtil.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . 'pi/carLocationSearch/Foundation/HttpReq.php';
 // require_once $_SERVER['DOCUMENT_ROOT'] . '/../../dbConfig/DatabaseConfig.php';
 // require_once $_SERVER['DOCUMENT_ROOT'] . '/dbConfig/DatabaseConfig.php';
 
